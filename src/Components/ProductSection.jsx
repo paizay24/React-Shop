ad4058988302c92f7a842feb9db7266e9ae5947f
paizay24 <paizay.dev24@gmail.com>
2024-12-14 21:59:54 +0630

    feat:responsive & container

--- src/Components/ProductSection.jsx
+++ src/Components/ProductSection.jsx
@@ -247,7 +247,7 @@ const products = [
   return (
     <div>
       <p className=' mt-3 text-gray-600'>Available Product Lists</p>
-     <div className=' grid grid-cols-3 gap-5'>
+     <div className=' grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-5'>
      { products.map((product) => (
         <ProductCard key={product.id} product={product}></ProductCard>
       ))}
--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,19 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import Header from './Components/Header'
-import Footer from './Components/Footer'
-import ProductCategorySection from './Components/ProductCategorySection'
-import ProductSection from './Components/ProductSection'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import Header from "./Components/Header";
+import Footer from "./Components/Footer";
+import ProductCategorySection from "./Components/ProductCategorySection";
+import ProductSection from "./Components/ProductSection";
+import Container from "./Components/Container";
 
-
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-   <Header></Header>
-   <ProductCategorySection></ProductCategorySection>
-   <ProductSection></ProductSection>
-   <Footer></Footer>
-  </StrictMode>,
-)
+    <Container>
+      <Header></Header>
+      <ProductCategorySection></ProductCategorySection>
+      <ProductSection></ProductSection>
+      <Footer></Footer>
+    </Container>
+  </StrictMode>
+);
